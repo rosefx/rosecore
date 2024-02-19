218cd209fc6a65458ca33da4874fa080ac42d634
Vitor Ribeiro <buss.vitorsheik@gmail.com>
2024-02-19 11:51:14 -0300

    [skip ci] move importer logic to Shared/Bundler

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,16 +5,18 @@ author 'Vitor Ribeiro <monk@codevitor.dev>';
 description 'Rose Core is a scalable and high-performance framework designed for various game mode servers on FiveM.';
 
 shared_scripts {
+
+  -- Modules;
   'Shared/sdk/version.lua',
   'Modules/misc/namespacer.lua',
   'Modules/tests/lust.lua',
   'Modules/misc/string.lua',
   'Modules/misc/file.lua',
 
-
-  'Core/shared/Index.lua',
-  'Core/shared/Importer.lua',
-  'Core/shared/Shared.lua'
+  -- Bundler;
+  'Shared/bundler/Index.lua',
+  'Shared/bundler/Importer.lua',
+  'Shared/bundler/Shared.lua'
 }
 
 core_env "development";
