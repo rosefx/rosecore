aff6694d58172a859bc6419c58d03e86ad276585
Vitor Ribeiro <buss.vitorsheik@gmail.com>
2024-02-22 07:39:05 -0300

    simplify the spec tests condition

--- spec/namespacer.spec.lua
+++ spec/namespacer.spec.lua
@@ -1,41 +1,37 @@
+if (ROSE_ENV ~= "development") then return end
 local describe, it, expect = lust.describe, lust.it, lust.expect
 
 
+describe("Namespacer Test Suite", function () 
+  it("should be able to create a namespace", function () 
+    local namespace = namespace:create("test-namespace");
+    expect(namespace).to.be.a("table");
+  end);
 
-if (ROSE_ENV == "development") then 
-  describe("Namespacer Test Suite", function () 
-    it("should be able to create a namespace", function () 
-      local namespace = namespace:create("test-namespace");
-      expect(namespace).to.be.a("table");
-    end)
-
-    it("should be able to destroy a namespace", function () 
-      local result = namespace:destroy("test-namespace");
-      expect(result).to.equal(true);
-    end);
-
-
-
-    it("should be able to create a class", function ()
-      local namespace = class:create("test-namespace");
-      expect(namespace).to.be.a("table");
-    end);
+  it("should be able to destroy a namespace", function () 
+    local result = namespace:destroy("test-namespace");
+    expect(result).to.equal(true);
+  end);
   
-    it("should not be able to create a class with an existing name", function () 
-      local namespace1 = class:create("test-namespace1");
-      local namespace2 = class:create("test-namespace1");
-  
-      expect(namespace1).to.be.a("table");
-      expect(namespace2).to.equal(false);
-    end);
-  
-    it("should be able to call methods", function () 
-      local namespace = class:create("test-methods");
-      namespace.public.test = true;
-      namespace.private.test = false;
-  
-      expect(namespace.public.test).to.equal(true);
-      expect(namespace.private.test).to.equal(false);
-    end);
-  end);  
-end
+  it("should be able to create a class", function ()
+    local namespace = class:create("test-namespace");
+    expect(namespace).to.be.a("table");
+  end);
+
+  it("should not be able to create a class with an existing name", function () 
+    local namespace1 = class:create("test-namespace1");
+    local namespace2 = class:create("test-namespace1");
+
+    expect(namespace1).to.be.a("table");
+    expect(namespace2).to.equal(false);
+  end);
+
+  it("should be able to call methods", function () 
+    local namespace = class:create("test-methods");
+    namespace.public.test = true;
+    namespace.private.test = false;
+
+    expect(namespace.public.test).to.equal(true);
+    expect(namespace.private.test).to.equal(false);
+  end);
+end);  
